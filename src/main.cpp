c3032b44af2f37f67857ce6a5382dd0a27949545
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-10-10 20:40:02 +0200

    Simple window initialisation.

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,33 @@
 using std::cout;
 using std::endl;
 
+
+void gameLoop() {
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+ 
+        // Clear screen
+        window.clear();
+ 
+        // Update the window
+        window.display();
+    }
+}
+void createWindow() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window")
+}
+
+
 auto main() -> int {
-    cout << "Hello, World" << endl;
+    createWindow();
+    gameLoop();
     return 0;
 }
